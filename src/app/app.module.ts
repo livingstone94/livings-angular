89d51d57ccaec2cff763485a21c1579590912e03
livingstone94 <livingstonemezah94@gmail.com>
2020-11-06 17:58:54 +0100

    forecast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './pages/add/add.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { ErrorComponent } from './ui/error/error.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    DetailComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    AddCardComponent,
+    ErrorComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
